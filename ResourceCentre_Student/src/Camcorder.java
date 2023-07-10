55ab7a1fc6858e140f84b146cfa1fcb81e488489
22015515 <22015515@TsukasaYuzaki123PC>
2023-07-10 22:01:54 +0800

    update viewAllChromebood() in ResourCentre

--- ResourceCentre_Student/src/Camcorder.java
+++ ResourceCentre_Student/src/Camcorder.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Camcorder extends Item{
 private int opticalZoom;
@@ -16,5 +17,30 @@ private int opticalZoom;
 		// Write your codes here
 		return output;
 	}
+	
+	public static void addCamcorder(ArrayList<Camcorder> camcorderList, Camcorder cc) {
+		Camcorder item;
+		for(int i = 0; i < camcorderList.size(); i++) {
+			item = camcorderList.get(i);
+			if (item.getAssetTag().equalsIgnoreCase(cc.getAssetTag()) )
+				return;
+		}
+
+		if ((cc.getAssetTag().isEmpty()) || (cc.getDescription().isEmpty()) ) {
+			return;
+		}
+
+		camcorderList.add(cc);
+		}
+	
+	public static Camcorder inputCamcorder() {
+		String tag = Helper.readString("Enter asset tag > ");
+		String description = Helper.readString("Enter description > ");
+		int zoom = Helper.readInt("Enter optical zoom > ");
+
+		Camcorder cc= new Camcorder(tag, description, zoom);
+		return cc;
+	}
+
 }
 
